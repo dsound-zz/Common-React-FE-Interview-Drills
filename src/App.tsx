bfb13f6bf18aeb068c32d7d453c5759683c59f87
Demian Sims <demiansims@gmail.com>
2025-12-02 19:35:35 -0500

    autocomplete-api wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,140 @@
-import { useState } from "react"
+import { useEffect, useRef, useState } from 'react';
+import { useDebounce } from './hooks/useDebounce';
 
-const items = Array.from({ length: 47 }, (_, i) => `Item ${i + 1}`)
-const ITEMS_PER_PAGE = 10
+const API_URL = 'http://localhost:3001/query?search='
+
+function highlighter(chars: string, query: string) {
+  const lower = chars.toLowerCase();
+  const q = query.toLowerCase();
+  const i = lower.indexOf(q);
+
+  if (i === -1) {
+    return { start: chars, match: "", end: "" };
+  }
+
+  return {
+    start: chars.slice(0, i),
+    match: chars.slice(i, i + query.length),
+    end: chars.slice(i + query.length),
+  };
+}
 
 function App() {
-  const [page, setPage] = useState<number>(1)
+  const [query, setQuery] = useState<string>('')
+  const [suggestions, setSuggestions] = useState<string[]>([])
+  const [loading, setLoading] = useState<boolean>(false)
+  const [error, setError] = useState<string | null>(null)
+  const [isOpen, setIsOpen] = useState<boolean>(false)
+  const [highlightIndex, setHighlightIndex] = useState<number>(-1)
+  const isSelectingRef = useRef(false)
+  const debouncedQuery = useDebounce(query, 300)
 
-  const paginator = () => {
-    const offset = (page - 1) * ITEMS_PER_PAGE 
 
-    return items.slice(offset, offset +  ITEMS_PER_PAGE)
+  const fetchQuery = async (): Promise<void> => {
+    setLoading(true)
+    setError(null)
+
+    try {
+      const res = await fetch(`${API_URL}${debouncedQuery}`)
+      const data = await res.json()
+      setSuggestions(data)
+    } catch (error) {
+      const errorMessage = error instanceof Error ? error.message : "There was an issue fetching"
+      setError(errorMessage)
+    } finally {
+      setLoading(false)
+    }
   }
 
-  const handlePrev = () => {
-    setPage(prev => prev - 1)
+  useEffect(() => {
+  // User clicked a suggestion
+  if (isSelectingRef.current) {
+    isSelectingRef.current = false;
+    return;
   }
 
-  const handleNext = () => {
-    setPage(prev => prev + 1)
+  // Empty query
+  if (!debouncedQuery.trim()) {
+    setIsOpen(false);
+    setSuggestions([]);
+    return;
   }
 
-console.log(items.length)
+  // Normal search flow
+  fetchQuery();
+  setIsOpen(true);
+}, [debouncedQuery]);
+
+  const handleKeyDown = (e: React.KeyboardEvent<HTMLInputElement>) => {
+
+    switch (e.key) {
+
+      case 'Enter':
+        if (highlightIndex >= 0 && highlightIndex < suggestions.length) {
+          e.preventDefault()
+          handleSuggestionClicked(suggestions[highlightIndex])
+        }
+        break
+
+      case 'Escape':
+        setIsOpen(false)
+        setHighlightIndex(-1)
+        break;
+
+      case 'ArrowDown':
+        e.preventDefault();
+        setIsOpen(true);
+        setHighlightIndex(i =>
+          i < suggestions.length - 1 ? i + 1 : 0
+        );
+        break;
+
+      case 'ArrowUp':
+        e.preventDefault();
+        setIsOpen(true);
+        setHighlightIndex(i =>
+          i > 0 ? i - 1 : suggestions.length - 1
+        );
+        break;
+
+      default:
+        break
+    }
+  }
+
+  useEffect(() => {
+    setHighlightIndex(-1);
+  }, [suggestions]);
+
+  const handleSuggestionClicked = (suggestion: string) => {
+    isSelectingRef.current = true
+    setQuery(suggestion)
+    setIsOpen(false)
+    setHighlightIndex(-1);
+  }
+  console.log(debouncedQuery, suggestions, isOpen)
   return (
     <div className="app">
-      <h1>Pagination</h1>
-      <div className="pages">
-        {paginator().map((page) => (
-          <div key={page}>{page}</div>
-        ))}
-      </div>
-      <div className="buttons">
-        <button disabled={page === 1} onClick={handlePrev}>Prev</button>
-        <div>current page: {page} number of pages {items.length}</div>
-        <button disabled={page === Math.ceil(items.length / ITEMS_PER_PAGE)} onClick={handleNext}>Next</button>
+      <h1>Autocomplete API (Boilerplate)</h1>
+
+      <div className="container">
+        {debouncedQuery.length && !suggestions.length ? <div>No results...</div> : null}
+        {loading ? <div>Loading...</div> : null}
+        {error ? <div>{error}</div> : null}
+        <input type="text" value={query} onChange={(e) => {
+          setQuery(e.target.value)
+          isSelectingRef.current = false
+        }} onKeyDown={handleKeyDown} />
+        {isOpen ? (
+          <ul className="suggestions">
+            {suggestions.map((suggestion) => {
+              const { start, match, end } = highlighter(suggestion, debouncedQuery)
+              return (
+                <li key={suggestion} onClick={() => handleSuggestionClicked(suggestion)}>{start}<strong>{match}</strong>{end}</li>
+              )
+            })}
+          </ul>
+        ) : null}
 
       </div>
     </div>
